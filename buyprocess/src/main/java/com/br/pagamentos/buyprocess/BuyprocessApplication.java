221084c57d52ff2637350c85457f470429fd57b7
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2019-11-24 21:28:47 -0300

    Renomeado o modulo de byprocess para buyprocess

---
+++ buyprocess/src/main/java/com/br/pagamentos/buyprocess/BuyprocessApplication.java
@@ -0,0 +1,15 @@
+package com.br.pagamentos.buyprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class BuyprocessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyprocessApplication.class, args);
+    }
+
+}
